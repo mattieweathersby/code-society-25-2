755c820211358189a21c27ba0e8c08f0cd0efe86
mattieweathersby <mweathersby252@gmail.com>
2025-08-19 15:30:20 +0000

    feat:adds mattieweathersby letter D equation homework

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,9 +2,20 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    return this.divide(this.multiply(a, b), this.pow(this.add(c, d), e));
   }
-
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
+  }
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
