6364bc6b6045cd6eff9869b60983b3d641603607
mattieweathersby <mweathersby252@gmail.com>
2025-08-28 17:51:08 +0000

    feat:implemented loader module and the mattie weathersby loader code, and all test passed

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { MattieWeathersbyLoader } from './mattie_weathersby_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  MattieWeathersbyLoader,
 ];
 
 @Module({
