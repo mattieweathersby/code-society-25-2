f539b7c166ec72c57a4b82df6c822cc1cf15f42f
mattieweathersby <mweathersby252@gmail.com>
2025-09-05 13:50:11 +0000

    feat:adds mattieweathersby lesson13 leetcode

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,21 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    int total = 0;
+
+    Map<Character, Integer> tMap = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      tMap.put(t.charAt(i), i);
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      char current = s.charAt(i);
+
+      int indexInT = tMap.get(current);
+
+      total += Math.abs(i - indexInT);
+    }
+
+    return total;
   }
 }
