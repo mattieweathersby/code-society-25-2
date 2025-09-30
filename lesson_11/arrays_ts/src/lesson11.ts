74ced51393bd334cd6f4e47bfb4be9dda08cb45f
mattieweathersby <mweathersby252@gmail.com>
2025-09-30 17:34:41 +0000

    bugfix: fixs the formatting in lesson11 typescript line 6

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,7 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-   const n = nums.length;
+    const n = nums.length;
     const ans: number[] = new Array(2 * n);
     for (let i = 0; i < n; i++) {
       ans[i] = nums[i];
