98a2a538c13ea2fc214e2e4594bae0b08f02223c
mattieweathersby <mweathersby252@gmail.com>
2025-10-02 15:05:04 +0000

    feat: adds contact form, linked with GET and POST method in server file also linked it to index.html file

--- lesson_21/mattieweathersby/server.js
+++ lesson_21/mattieweathersby/server.js
@@ -1,18 +1,25 @@
-//import the http module to create a server
-const http = require('http');
-
 //import the express module to handle routing
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send(`Server is running on port ${PORT}\n`);
+    res.sendFile(path.join(__dirname, '/public/contact.html'));
 });
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Handle form submission from contact.html
+app.post('/contact', (req, res) => {
+  console.log(req.body); // see the submitted data in terminal
+  res.send(`Thanks, ${req.body.name}! We received your message: ${req.body.message}`);
+});
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Express server is running on port ${PORT}`);
